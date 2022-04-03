91843f852c927e171b719ac1d4479e7f0fcd422d
Arin Agarwal <arin.agarwal02@gmail.com>
2022-04-03 23:50:56 +0530

    Update 69_Sqrt(x).cpp

--- easy/69_Sqrt(x).cpp
+++ easy/69_Sqrt(x).cpp
@@ -16,3 +16,24 @@ public:
         else return y;
     }
 };
+
+//simple one
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        long int ans=1,y=0;
+        
+        while(ans){
+            if((ans*ans)<=x){
+                ans++;
+            }
+            else
+                return ans-1;
+        }
+        return ans;
+        // if(ans*ans<=x)
+        //     return ans;
+        // else return y;
+    }
+};
