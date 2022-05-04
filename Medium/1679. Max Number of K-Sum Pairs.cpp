da775a8662b18c9aeade6dae3b26b699901750e6
Arin Agarwal <arin.agarwal02@gmail.com>
2022-05-04 23:12:12 +0530

    Create 1679. Max Number of K-Sum Pairs.cpp

---
+++ Medium/1679. Max Number of K-Sum Pairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:    
+    int maxOperations(vector<int>& nums, int k) {
+        int n= nums.size();
+        int i=0,j=n-1,c=0;
+        sort(nums.begin(),nums.end());
+        
+        while(i<j)
+        {
+            if(nums[i]+nums[j]==k)
+            {
+                c++;
+                i++;
+                j--;
+            }
+            
+            else if(nums[i]+nums[j]<k)
+                i++;
+            else
+                j--;
+        }
+        return c;
+    }
+};
