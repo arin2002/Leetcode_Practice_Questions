d5f88f70b226d678536319e97106990e3fb74604
Arin Agarwal <arin.agarwal02@gmail.com>
2022-05-06 00:12:25 +0530

    Create 225. Implement Stack using Queues.cpp

---
+++ Medium/225. Implement Stack using Queues.cpp
@@ -0,0 +1,37 @@
+class MyStack{
+public:
+    MyStack() {
+        
+    }
+    queue<int> q;
+    void push(int x) 
+    {
+        q.push(x);
+        for(int i=0;i<q.size()-1;i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        
+    }
+    
+    int pop() 
+    {
+        int temp=q.front();
+        q.pop();
+        return temp;
+        
+    }
+    
+    int top()
+    {
+        return q.front();
+        
+    }
+    
+    bool empty()
+    {
+        return q.empty();
+        
+    }
+};
